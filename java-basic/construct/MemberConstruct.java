677a268fd52ba0fff3d987c81a207ce806630a2c
Youmee <dbaldpahd0216@naver.com>
2024-12-23 11:58:27 +0900

    add codes

--- java-basic/construct/MemberConstruct.java
+++ java-basic/construct/MemberConstruct.java
@@ -1,11 +1,16 @@
 package construct;
 
-import javax.swing.text.html.parser.TagElement;
-
 public class MemberConstruct {
     String name;
     int age;
     int grade;
+
+    //추가
+    MemberConstruct(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.grade = 50;
+    }
 
     MemberConstruct(String name, int age, int grade){
         System.out.println("생성자 호출 name = " + name + ", age = " + age + ", grade = " + grade);
