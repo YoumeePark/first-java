efeb2074401abbc75320b7b561f059e2df9c43d6
Youmee <137977582+YoumeePark@users.noreply.github.com>
2024-12-29 16:58:04 +0900

    add codes

--- java-basic/static2/DecoDataMain.java
+++ java-basic/static2/DecoDataMain.java
@@ -1,5 +1,8 @@
 package static2;
 
+//import static static2.DecoData.staticCall;
+//import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
