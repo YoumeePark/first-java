7b6c7234f540726b6e32c3848aa2714ad90fa422
Youmee <dbaldpahd0216@naver.com>
2025-01-13 14:11:47 +0900

    add codes

---
+++ java-mid1/src/lang/object/tostring/Dog.java
@@ -0,0 +1,12 @@
+package lang.object.tostring;
+
+public class Dog {
+    private String dogName;
+    private int age;
+
+    public Dog(String dogName) {
+
+        this.dogName = dogName;
+        this.age = age;
+    }
+}
