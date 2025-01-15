ad64e1007bf14ab06c7c2b06c416ff888fe706dc
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-01-15 18:49:59 +0900

    Immutable

--- java-mid1/lang/imutable/address/RefMain1_1.java
+++ java-mid1/lang/imutable/address/RefMain1_1.java
@@ -10,7 +10,7 @@ public class RefMain1_1 {
 
         b.setValue("부산"); //b의 값을 부산으로 변경해야함
         System.out.println("부산 -> b");
-        System.out.println("a = " + a);
+        System.out.println("a = " + a); //사이드 이펙트 발생
         System.out.println("b = " + b);
     }
 }
