c609a79cf03d3fd7e4e7564447eb98b85f99ac6e
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-01-24 17:34:45 +0900

    Enumeration

---
+++ java-mid1/enumeration/ex2/StringGradeEx2_2.java
@@ -0,0 +1,14 @@
+package enumeration.ex2;
+
+public class StringGradeEx2_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        /*
+        ClassGrade newclassGrade = new ClassGrade(); //x009
+        int result = discountService.discount(newclassGrade, price);
+        System.out.println("newClassGrade 등급의 할인 금액: " + result);
+         */
+    }
+}
