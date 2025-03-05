a1a6ee500737b5e7fe3b3efdbd25d0c3d84c63f5
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-03-05 17:46:24 +0900

    collection.link.MyLinkedList

--- java-mid2/collection/link/Node.java
+++ java-mid2/collection/link/Node.java
@@ -7,4 +7,33 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+    /*
+    //IDE 생성 toString()
+    @Override
+    public String toString() {
+        return "Node{" +
+                "item=" + item +
+                ", next=" + next +
+                '}';
+    }*/
+
+    //[A->B->C]
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(); //루프에서 문자를 더할 때
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+        /*String string = sb.toString();
+        return string;*/
+    }
 }
