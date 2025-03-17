cad50b53d0722d67bf2014f97c24757796619989
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-03-17 18:08:38 +0900

    collection.set

---
+++ java-mid2/collection/set/MyHashSetV2.java
@@ -0,0 +1,79 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV2 {
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    private LinkedList<Object>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV2() {
+        initBuckets();
+    }
+
+    public MyHashSetV2(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(Object value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) { //O(n) -> O(1) 데이터가 1~2개만 들어있음
+            return false;
+        }
+
+        bucket.add(value);
+        size++;
+        return true;
+    }
+
+    public boolean contains(Object searchValue) {
+        int hashIndex = hashIndex(searchValue);  //O(1)
+        LinkedList<Object> bucket = buckets[hashIndex]; //O(1)
+        return bucket.contains(searchValue); //O(n) -> 하지만 사실상 O(1)에 가깝다고 볼 수 있다.
+    }
+
+    public boolean remove(Object value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Object> bucket = buckets[hashIndex];
+        boolean result = bucket.remove(value);
+        if (result) {
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private int hashIndex(Object value) {
+        // -1, -10 -> 1, 10
+        //hashCode의 결과로 음수가 나올 수 있다. abs()를 사용해서 마이너스를 제거한다.
+        return Math.abs(value.hashCode()) % capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV2{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
