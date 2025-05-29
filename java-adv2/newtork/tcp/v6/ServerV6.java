d13d7efa55035025c4d9c28c35197bf8bd973d84
Youmee <137977582+YoumeePark@users.noreply.github.com>
2025-05-29 16:37:24 +0900

    network.tcp.v6

---
+++ java-adv2/newtork/tcp/v6/ServerV6.java
@@ -0,0 +1,57 @@
+package newtork.tcp.v6;
+
+import newtork.tcp.v5.SessionV5;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class ServerV6 {
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("서버 시작");
+        SessionManagerV6 sessionManager = new SessionManagerV6();
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        log("서버 소켓 시작 - 리스닝 포트: " + PORT);
+
+        // ShutdownHook 등록
+        ShutdownHook shutdownHook = new ShutdownHook(serverSocket, sessionManager);
+        Runtime.getRuntime().addShutdownHook(new Thread(shutdownHook, "shutdown"));
+        while (true) {
+            Socket socket = serverSocket.accept(); // 블로킹
+            log("소켓 연결: " + socket);
+
+            SessionV5 session = new SessionV5(socket);
+            Thread thread = new Thread(session);
+            thread.start();
+        }
+    }
+
+    static class ShutdownHook implements Runnable {
+
+        private final ServerSocket serverSocket;
+        private final SessionManagerV6 sessionManager;
+
+        ShutdownHook(ServerSocket serverSocket, SessionManagerV6 sessionManager) {
+            this.serverSocket = serverSocket;
+            this.sessionManager = sessionManager;
+        }
+
+        @Override
+        public void run() {
+            log("shutdownHook 실행");
+            try {
+                sessionManager.closeAll();
+                serverSocket.close();
+
+                Thread.sleep(1000); // 자원 정리 대기
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("e = " + e);
+            }
+        }
+    }
+}
